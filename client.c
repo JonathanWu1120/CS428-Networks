a21bf8c495c83bbe52ad25d518031b4a9ed11e96
jojo <jonathanwu3572@gmail.com>
2019-03-19 20:00:07 -0400

    Works for 1 client

--- client.c
+++ client.c
@@ -1,148 +1,66 @@
-#include <sys/socket.h>
-#include <stdlib.h>	
-#include <stdio.h>	
-#include <string.h>
-#include <netinet/in.h>	
-#include <string.h>
-#include <netdb.h>
-#include "port.h"
-#define MAXSIZE 1024
-int conn(char *host,int port);
-void disconn(fd);
-int debug = 1;
 
-int main(int argc, char**argv){
-	extern char *optarg;
-	extern int optind;
-	int c, err = 0; 
-	char *prompt = 0;
-	int port = SERVICE_PORT;	
-	char *host = "localhost";	
-	int fd;				
-	char buffer[1024];
-	char buff[1024];
-	int num;
-	static char usage[] = 
-	              "usage: %s [-d] [-h serverhost] [-p port]\n";
+// Write CPP code here 
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+void func(int sockfd) 
+{ 
+    char buff[MAX]; 
+    int n; 
+    for (;;) { 
+        bzero(buff, sizeof(buff)); 
+        printf("Enter the string : "); 
+        n = 0; 
+        while ((buff[n++] = getchar()) != '\n') 
+            ; 
+        write(sockfd, buff, sizeof(buff)); 
+        bzero(buff, sizeof(buff)); 
+        read(sockfd, buff, sizeof(buff)); 
+        printf("From Server : %s", buff); 
+        if ((strncmp(buff, "exit", 4)) == 0) { 
+            printf("Client Exit...\n"); 
+            break; 
+        } 
+    } 
+} 
+  
+int main() 
+{ 
+    int sockfd, connfd; 
+    struct sockaddr_in servaddr, cli; 
+  
+    // socket create and varification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+  
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    servaddr.sin_port = htons(PORT); 
+  
+    // connect the client socket to server socket 
+    if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+        printf("connection with the server failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("connected to the server..\n"); 
+  
+    // function for chat 
+    func(sockfd); 
+  
+    // close the socket 
+    close(sockfd); 
+} 
 
-	while ((c = getopt(argc, argv, "dh:p:")) != -1)
-		switch (c) {
-		case 'h':  
-			host = optarg;
-			break;
-		case 'p': 
-			port = atoi(optarg);
-			if (port < 1024 || port > 65535) {
-				fprintf(stderr, "invalid port number: %s\n", optarg);
-				err = 1;
-			}
-			break;
-		case '?':
-			err = 1;
-			break;
-		}
-	if (err || (optind < argc)) {
-		fprintf(stderr, usage, argv[0]);
-		exit(1);
-	}
-
-	printf("connecting to %s, port %d\n", host, port);
-
-	if ((fd = conn(host, port)) < 0)    
-		exit(1);   
-
-	/* in a useful program, we would do something here involving reads and writes on fd */
-	while(1){
-		printf("Client: Enter data for server side:\n");
-		fgets(buffer,MAXSIZE-1,stdin);
-		if((send(fd,buffer,strlen(buffer),0)) == -1){
-			fprintf(stderr, "Message faield to send\n");
-			disconn(fd);
-			exit(1);
-		}else{
-			printf("Client: Message being sent: %s\n",buffer);
-			num = recv(fd, buffer, sizeof(buffer),0);
-			if (num <= 0){
-				printf("Error occured\n");
-				break;
-			}
-			buff[num] = '\0';
-			printf("Client:Message received from server: %s\n", buffer);
-		}
-	}
-
-	disconn(fd);   
-	return 0;
-}
-
-
-int conn(char *host, int port){
-	struct hostent *hp;	/* host information */
-	unsigned int alen;	/* address length when we get the port number */
-	struct sockaddr_in myaddr;	/* our address */
-	struct sockaddr_in servaddr;	/* server address */
-	int fd;  /* fd is the file descriptor for the connected socket */
-
-	if (debug) printf("conn(host=\"%s\", port=\"%d\")\n", host, port);
-
-	if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("cannot create socket");
-		return -1;
-	}
-
-	memset((char *)&myaddr, 0, sizeof(myaddr));
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	myaddr.sin_port = htons(0);
-
-	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
-		perror("bind failed");
-		close(fd);
-		return -1;
-	}
-
-	/* this part is for debugging only - get the port # that the operating */
-	/* system allocated for us. */
-        alen = sizeof(myaddr);
-        if (getsockname(fd, (struct sockaddr *)&myaddr, &alen) < 0) {
-                perror("getsockname failed");
-		close(fd);
-		return -1;
-        }
-	if (debug) printf("local port number = %d\n", ntohs(myaddr.sin_port));
-
-	/* fill in the server's address and data */
-	/* htons() converts a short integer to a network representation */
-
-	memset((char*)&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(port);
-
-	/* look up the address of the server given its name */
-	hp = gethostbyname(host);
-	if (!hp) {
-		fprintf(stderr, "could not obtain address of %s\n", host);
-		close(fd);
-		return -1;
-	}
-
-	/* put the host's address into the server address structure */
-	memcpy((void *)&servaddr.sin_addr, hp->h_addr_list[0], hp->h_length);
-
-	/* connect to server */
-	if (connect(fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0) {
-		perror("connect failed");
-		close(fd);
-		return -1;
-	}
-	if (debug) printf("connected socket = %d\n", fd);
-	return fd;
-}
-
-/* disconnect from the service */
-/* lame: we can just as easily do a shutdown() or close() ourselves */
-
-void disconn(int fd){
-	if (debug) printf("disconn(%d)\n", fd);
-	shutdown(fd, 2);    /* 2 means future sends & receives are disallowed */
-}
--- server.c
+++ server.c
@@ -1,138 +1,95 @@
-#include <sys/socket.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/errno.h>
-#include <sys/wait.h>
-#include "port.h"
+#include <netdb.h> 
+#include <netinet/in.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#include <sys/types.h> 
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+  
+// Function designed for chat between client and server. 
+void func(int sockfd) 
+{ 
+    char buff[MAX]; 
+    int n; 
+    // infinite loop for chat 
+    for (;;) { 
+        bzero(buff, MAX); 
+  
+        // read the message from client and copy it in buffer 
+        read(sockfd, buff, sizeof(buff)); 
+        // print buffer which contains the client contents 
+        printf("From client: %s\t To client : ", buff); 
+        bzero(buff, MAX); 
+        n = 0; 
+        // copy server message in the buffer 
+        while ((buff[n++] = getchar()) != '\n') 
+            ; 
+  
+        // and send that buffer to client 
+        write(sockfd, buff, sizeof(buff)); 
+  
+        // if msg contains "Exit" then server exit and chat ended. 
+        if (strncmp("exit", buff, 4) == 0) { 
+            printf("Server Exit...\n"); 
+            break; 
+        } 
+    } 
+} 
+  
+// Driver function 
+int main() 
+{ 
+    int sockfd, connfd, len; 
+    struct sockaddr_in servaddr, cli; 
+  
+    // socket create and verification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+  
+    // assign IP, PORT 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(PORT); 
+  
+    // Binding newly created socket to given IP and verification 
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+        printf("socket bind failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully binded..\n"); 
+  
+    // Now server is ready to listen and verification 
+    if ((listen(sockfd, 5)) != 0) { 
+        printf("Listen failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Server listening..\n"); 
+    len = sizeof(cli); 
+  
+    // Accept the data packet from client and verification 
+    connfd = accept(sockfd, (SA*)&cli, &len); 
+    if (connfd < 0) { 
+        printf("server acccept failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("server acccept the client...\n"); 
+  
+    // Function for chatting between client and server 
+    func(connfd); 
+  
+    // After chatting close the socket 
+    close(sockfd); 
+} 
 
-#ifndef ERESTART
-#define ERESTART EINTR
-#endif
-
-extern int errno;
-void serve(int port);
-
-int main(int argc, char ** argv){
-	extern char *optarg;
-	extern int optind;
-	int c, err = 0; 
-	int port = SERVICE_PORT;
-	static char usage[] = "usage: %s [-d] [-p port]\n";
-
-	while ((c = getopt(argc, argv, "dp:")) != -1)
-		switch (c) {
-		case 'p':
-			port = atoi(optarg);
-			if (port < 1024 || port > 65535) {
-				fprintf(stderr, "invalid port number: %s\n", optarg);
-				err = 1;
-			}
-			break;
-		case '?':
-			err = 1;
-			break;
-		}
-	if (err || (optind < argc)) {
-		fprintf(stderr, usage, argv[0]);
-		exit(1);
-	}
-	serve(port);
-}
-
-
-void serve(int port){
-	int svc;        /* listening socket providing service */
-	int rqst;       /* socket accepting the request */
-	socklen_t alen;       /* length of address structure */
-	struct sockaddr_in my_addr;    /* address of this service */
-	struct sockaddr_in client_addr;  /* client's address */
-	int sockoptval = 1;
-	char hostname[128]; /* host name, for debugging */
-	int num;
-
-	gethostname(hostname, 128);
-
-	/* get a tcp/ip socket */
-	/*   AF_INET is the Internet address (protocol) family  */
-	/*   with SOCK_STREAM we ask for a sequenced, reliable, two-way */
-	/*   conenction based on byte streams.  With IP, this means that */
-	/*   TCP will be used */
-
-	if ((svc = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-		perror("cannot create socket");
-		exit(1);
-	}
-
-	/* we use setsockopt to set SO_REUSEADDR. This allows us */
-	/* to reuse the port immediately as soon as the service exits. */
-	/* Some operating systems will not allow immediate reuse */
-	/* on the chance that some packets may still be en route */
-	/* to the port. */
-
-	setsockopt(svc, SOL_SOCKET, SO_REUSEADDR, &sockoptval, sizeof(int));
-
-	/* set up our address */
-	/* htons converts a short integer into the network representation */
-	/* htonl converts a long integer into the network representation */
-	/* INADDR_ANY is the special IP address 0.0.0.0 which binds the */
-	/* transport endpoint to all IP addresses on the machine. */
-
-	memset((char*)&my_addr, 0, sizeof(my_addr));  /* 0 out the structure */
-	my_addr.sin_family = AF_INET;   /* address family */
-	my_addr.sin_port = htons(port);
-	my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	/* bind to the address to which the service will be offered */
-	if (bind(svc, (struct sockaddr *)&my_addr, sizeof(my_addr)) < 0) {
-		perror("bind failed");
-		exit(1);
-	}
-
-	/* set up the socket for listening with a queue length of 5 */
-	if (listen(svc, 5) < 0) {
-		perror("listen failed");
-		exit(1);
-	}
-
-	printf("server started on %s, listening on port %d\n", hostname, port);
-
-	/* loop forever - wait for connection requests and perform the service */
-	alen = sizeof(client_addr);     /* length of address */
-	char buffer[10241];
-	char *buff;
-	for (;;) {
-		while ((rqst = accept(svc,
-		                (struct sockaddr *)&client_addr, &alen)) < 0) {
-			/* we may break out of accept if the system call */
-			/* was interrupted. In this case, loop back and */
-			/* try again */
-			if ((errno != ECHILD) && (errno != ERESTART) && (errno != EINTR)) {
-				perror("accept failed");
-				exit(1);
-			}
-		}
-
-		printf("received a connection from: %s port %d\n",
-			inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-		if ((num = recv(rqst,buffer,1024,0))==-1){
-			perror("recv");
-			exit(1);
-		}else if(num ==0){
-			printf("Connection closed\n");
-			break;
-		}
-		buffer[num] = '\0';
-		printf("Server: Msg received %s \n", buffer);
-		if((send(rqst,buffer,strlen(buffer),0))==-1){
-			fprintf(stderr,"Failure sending message\n");
-			shutdown(rqst,2);
-			break;
-		}
-		printf("Server:Msg being sent: %s\nNumber of bytes send %d\n",buffer,strlen(buffer));
-        	shutdown(rqst, 2);    /* close the connection */
-	}
-	close(svc);
-}
