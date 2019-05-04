76d5eb37f98b7ac061acbc81c6402402062dfb6f
jojo <jonathanwu3572@gmail.com>
2019-05-04 16:32:07 -0400

    New approach

--- prog2/client.c
+++ prog2/client.c
@@ -1,56 +1,125 @@
-#include <sys/socket.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include "utils.h"
 
-#define PORT_NUMBER 5000
-#define SERVER_ADDR "192.168.1.7"
-#define FILENAME "/home/hello.c"
+int main (int argc, char **argv) {
+    struct addrinfo hints, *server_info, *my_info;
+    int socketFD;
+    const char *inputFileName = argv[4];
+    FILE *inputFile;
 
-int main(int argc, char **argv){
-	int client_socket;
-	ssize_t len;
-	struct sockaddr_in remote_addr;
-	char buffer[BUFSIZ];
-	int file_size;
-	FILE *received_file;
-	int remain_data = 0;
-	
-	memset(&remote_addr, 0, sizeof(remote_addr));
-	remote_addr.sin_family = AF_INET;
-	inet_pton(AF_INET, SERVER_ADDR, &(remote_addr.sin_addr));
-	remote_addr.sin_port = htons(PORT_NUMBER);
+    // Ensure that the correct number of command line arguments were provided
+    if (argc != 5) {
+        printf("Usage: deliver <server address> <server port number> <client listen port> <file name>\n");
+        return 1;
+    }
 
-	client_socket = socket(AF_INET, SOCK_STREAM,0);
-	if(client_socket == -1){
-		fprintf(stderr, "ERROR CREATING SOCKET %s", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	if(connect(client_socket, (struct sockaddr *)&remote_addr, sizeof(struct sockaddr))==-1){
-		fprintf(stderr,"Failed to connect %s", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	recv(client_socket, buffer, BUFSIZ, 0);
-	file_size = atoi(buffer);
+    memset (&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_flags = AI_PASSIVE;
 
-	received_file = fopen(FILENAME, "W");
-	if(received_file == NULL){
-		fprintf(stderr, "Failed opening file %s\n", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	remain_data = file_size;
-	while((remain_data > 0) && ((len = recv(client_socket, buffer, BUFSIZ, 0)) > 0)){
-		fwrite(buffer, sizeof(char), len, received_file);
-		remain_data -= len;
-		printf("Received %ld bytes and %d remaining\n",len, remain_data);
-	}
-	fclose(received_file);
-	close(client_socket);
-	return 0;
+    // Get IP address of the server
+    getaddrinfo(argv[1], argv[2], &hints, &server_info);
+
+    // Get client's IP address
+    getaddrinfo(NULL, argv[3], &hints, &my_info);
+
+    if (server_info != NULL) {
+
+        // Create socket
+        socketFD = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+
+        struct timeval tv;
+
+        // 1-second timeout on recv calls with socket
+        tv.tv_sec = 1;
+
+        // Not initialzingthis can cause strange errors
+        tv.tv_usec = 0;
+        setsockopt(socketFD, SOL_SOCKET, SO_RCVTIMEO, (char *)&tv,sizeof(struct timeval));
+
+        bind(socketFD, my_info->ai_addr, my_info->ai_addrlen);
+
+        inputFile = fopen(inputFileName, "rb");
+
+        struct stat inputFileInfo;
+
+        // Get filesize to calculate total number of fragments and total number of fragment digits
+        stat(inputFileName, &inputFileInfo);
+
+        unsigned int total_frag = ((inputFileInfo.st_size) / MAX_DATA_SIZE) + 1;
+        unsigned int current_frag_no = 1;
+
+        char *byteArrayPacket;
+        Packet currentPacket;
+        currentPacket.total_frag = total_frag;
+        strncpy (currentPacket.filename, inputFileName, strlen(inputFileName) + 1);
+
+        while (current_frag_no <= total_frag) {
+            currentPacket.frag_no = current_frag_no;
+
+            bool endOfFile = feof(inputFile);
+            char nextChar;
+
+            unsigned int i;
+            for (i = 0; i < MAX_DATA_SIZE && !endOfFile; i++) {
+                nextChar = getc (inputFile);
+                if (feof(inputFile)) {
+                    endOfFile = true;
+                    i--;
+                } else {
+                    currentPacket.filedata[i] = nextChar;
+                }
+            }
+
+            currentPacket.filedata[i] = '\0';
+            currentPacket.size = i;
+
+            unsigned int currentPacketTotalSize = createPacket (&byteArrayPacket, &currentPacket);
+
+            char buf[BUFLEN];
+            int ackReceived = 0;
+            Packet ackPacket;
+
+            do {
+                //Transmit the packet that was just created
+                int bytesSent = sendto(socketFD, byteArrayPacket, currentPacketTotalSize, 0, server_info->ai_addr, sizeof(struct sockaddr_storage));
+
+                struct sockaddr_storage dummyVar;
+                int dummyVar_len = sizeof(dummyVar);
+
+                // Wait 1 second for ACK packet from server
+                int bytesReceived = recvfrom(socketFD, buf, BUFLEN, 0, (struct sockaddr *) &dummyVar, &dummyVar_len);
+
+                if (bytesReceived != -1) {
+                    ackReceived = 1;
+                    memset(&ackPacket,0, sizeof(Packet));
+                    extractPacket(&ackPacket, buf, bytesReceived);
+                    current_frag_no = ackPacket.frag_no;
+                }
+
+            } while(ackReceived == 0 && current_frag_no <= total_frag);
+
+            free(byteArrayPacket);
+        }
+
+        //Free the server_info and my_info linked lists from getaddrinfo calls
+        freeaddrinfo(server_info);
+        freeaddrinfo(my_info);
+    }
+
+    return 0;
 }
 
